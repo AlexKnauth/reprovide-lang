e04662457dcc16ce3e3bab5e6605a364564abc4f
AlexKnauth <alexander@knauth.org>
2015-08-27 14:10:25 -0400

    add rackunit-lib build-dep and use module+ test

--- reprovide/test/test.rkt
+++ reprovide/test/test.rkt
@@ -1,9 +1,11 @@
 #lang racket/base
-(require rackunit "use.rkt")
-(check-equal? (first '(1 2 3)) 1)
-(check-equal? (match '(1 2 3) [(list a _ _) a]) 1)
-(define-syntax m
-  (lambda (stx)
-    (syntax-parse stx
-      [_ #'5])))
-(check-equal? (m) 5)
+(module+ test
+  (require rackunit "use.rkt")
+  (check-equal? (first '(1 2 3)) 1)
+  (check-equal? (match '(1 2 3) [(list a _ _) a]) 1)
+  (define-syntax m
+    (lambda (stx)
+      (syntax-parse stx
+        [_ #'5])))
+  (check-equal? (m) 5)
+  )
