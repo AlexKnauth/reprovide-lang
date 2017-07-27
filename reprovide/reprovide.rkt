2fe755d9416bb38d55b240220581d7a6d0141b71
AlexKnauth <alexander@knauth.org>
2017-07-27 13:31:30 -0400

    use reprov-spec syntax class

--- reprovide/reprovide.rkt
+++ reprovide/reprovide.rkt
@@ -2,21 +2,20 @@
 
 (provide reprovide)
 
-(require (for-syntax racket/base
-                     racket/list
-                     racket/require-transform
-                     syntax/parse))
+(require syntax/parse/define
+         (for-syntax racket/base
+                     racket/require-transform))
 
-(define-syntax reprovide
-  (syntax-parser
-    [(reprovide require-spec ...)
-     #:with [[[local-id phase] ...] ...]
-     (for/list ([require-spec (in-list (syntax->list #'(require-spec ...)))])
-       (define-values [imports import-sources]
-         (expand-import require-spec))
-       (for/list ([import (in-list imports)])
-         (list (import-local-id import)
-               (import-mode import))))
-     #'(begin
-         (require require-spec ...)
-         (provide (for-meta phase local-id) ... ...))]))
+(begin-for-syntax
+  (define-syntax-class reprov-spec
+    [pattern require-spec:expr
+      #:with [[local-id phase] ...]
+      (let-values ([[imports import-sources] (expand-import #'require-spec)])
+        (for/list ([import (in-list imports)])
+          (list (import-local-id import)
+                (import-mode import))))]))
+
+(define-simple-macro (reprovide spec:reprov-spec ...)
+  (begin
+    (require spec.require-spec ...)
+    (provide (for-meta spec.phase spec.local-id) ... ...)))
